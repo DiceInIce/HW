071eb62738fdbf5d153e8956064336f8412fa6cd
DiceInIce <germanikan20@gmail.com>
2025-11-15 15:52:55 +0300

    LINQ

---
+++ LINQ_HW/LINQ_HW/Program.cs
@@ -0,0 +1,158 @@
+﻿using System.Linq;
+
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string City { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Name} {Age} {City}";
+    }
+
+}
+
+public class Product
+{
+    public string Name { get; set; }
+    public string Category { get; set; }
+    public decimal Price { get; set; }
+    public int Stock { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Name} {Category} {Price} {Stock}";
+    }
+}
+
+internal class Program
+{
+
+
+    static void Main(string[] args)
+    {
+        void PrintNums(IEnumerable<int> nums) { foreach (int i in nums) { Console.Write($"{i} "); } }
+        void PrintPeoples(IEnumerable<Person> people) { foreach (Person p in people) { Console.WriteLine(p.ToString()); } }
+        void PrintProducts(IEnumerable<Product> products) { foreach (Product p in products) { Console.WriteLine(p.ToString()); } }
+
+        Console.WriteLine("Задание 1\n"); // 1111111111111
+
+        int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+
+        var evenNums = from n in numbers
+                       where n % 2 == 0
+                       select n;
+        Console.WriteLine("Четные сила :");
+        PrintNums(evenNums);
+
+
+        var someNums = from n in numbers
+                       where n > 5 && n < 15
+                       select n;
+        Console.WriteLine("\nЧисла больше 5 и меньше 15:"); PrintNums(someNums);
+
+
+        var sumNums = numbers.Sum();
+        Console.WriteLine($"\nСумма чисел: {sumNums.ToString()}");
+
+
+        int maxNum = numbers.Max();
+        int minNum = numbers.Min();
+        Console.WriteLine($"Максимальное и минимальное: {maxNum.ToString()}, {minNum.ToString()}");
+
+
+        var sortedDescNums = numbers.OrderDescending();
+        Console.WriteLine("Сортировка по убыванию: "); PrintNums(sortedDescNums);
+
+
+        Console.WriteLine("\n\nЗадание 2\n"); // 2222222222222
+
+        List<Person> people = new List<Person> {
+            new Person { Name = "Анна", Age = 25, City = "Москва" },
+            new Person { Name = "Иван", Age = 30, City = "Санкт-Петербург" },
+            new Person { Name = "Мария", Age = 22, City = "Москва" },
+            new Person { Name = "Петр", Age = 35, City = "Казань" },
+            new Person { Name = "Ольга", Age = 28, City = "Москва" }
+        };
+
+
+        var peopleOver25 = from p in people
+                           where p.Age > 25
+                           select p;
+        Console.WriteLine("\nСтарше 25: "); PrintPeoples(peopleOver25);
+
+
+        var peopleFromMoscow = from p in people
+                               where p.City == "Москва"
+                               select p;
+        Console.WriteLine("\nЛюди из Москвы: "); PrintPeoples(peopleFromMoscow);
+
+
+        var groupedByCity = people.GroupBy(p => p.City);
+        Console.WriteLine("\nГруппировка по городам: ");
+        foreach (var group in groupedByCity)
+        {
+            Console.WriteLine($"Город: {group.Key}");
+
+            foreach (var person in group) Console.WriteLine(person.ToString());
+
+            Console.WriteLine();
+        }
+
+
+        var averageAge = people.Average(p => p.Age);
+        Console.WriteLine($"\nСредний возраст: {averageAge}");
+
+
+        var sortedDescAge = people.OrderByDescending(p => p.Age);
+        Console.WriteLine("\nСортировка по убыванию возраста: "); PrintPeoples(sortedDescAge);
+
+
+        Console.WriteLine("\n\nЗадание 3\n"); //33333333333333
+
+        List<Product> products = new List<Product>
+        {
+            new Product { Name = "Ноутбук", Category = "Электроника", Price = 50000, Stock = 10 },
+            new Product { Name = "Мышь", Category = "Электроника", Price = 1500, Stock = 25 },
+            new Product { Name = "Стул", Category = "Мебель", Price = 8000, Stock = 15 },
+            new Product { Name = "Стол", Category = "Мебель", Price = 12000, Stock = 8 },
+            new Product { Name = "Клавиатура", Category = "Электроника", Price = 3000, Stock = 0 }
+        };
+
+
+        var missingProducts = from p in products
+                              where p.Stock == 0
+                              select p;
+        Console.WriteLine("Продукты которых нет в наличии:"); PrintProducts(missingProducts);
+
+
+        var groupedCategory = products.GroupBy(p => p.Category);
+
+        Console.WriteLine("\n\nГруппировка по категории:\n");
+        foreach (var category in groupedCategory)
+        {
+            Console.WriteLine($"Категория: {category.Key}");
+
+            var prodCount = category.Sum(p => p.Stock);
+            Console.WriteLine($"Количетсво товара: {prodCount}");
+
+            var averagePrice = category.Average(p => p.Price);
+            Console.WriteLine($"Средняя цена: {(int)averagePrice}");
+
+            var mostEpensive = category.OrderByDescending(p => p.Price).FirstOrDefault();
+            Console.WriteLine($"Самый дорогой товар: {mostEpensive?.ToString()}");
+
+            Console.WriteLine();
+        }
+
+
+        var productsOver2000 = products.Where(p => p.Price > 2000).OrderByDescending(p => p.Price);
+        Console.WriteLine("\n\nТовары дороже 2000 и сортированные по убыванию:\n");
+        PrintProducts(productsOver2000);
+
+
+        var allCost = products.Sum(p => p.Price * p.Stock);
+        Console.WriteLine($"\n\nОбщая стоимсть всех товаров на складе: {(int)allCost}\n");
+    }
+}
---
+++ LINQ_HW/LINQ_HW_2/Program.cs
@@ -0,0 +1,180 @@
+﻿using System.Linq;
+
+public class Order
+{
+    public int OrderId { get; set; }
+    public string CustomerName { get; set; }
+    public decimal TotalAmount { get; set; }
+
+    public override string ToString()
+    {
+        return $"{OrderId} {CustomerName} {TotalAmount}";
+    }
+}
+
+public class Customer
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string City { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Name} {Email} {City}";
+    }
+}
+
+public class Student
+{
+    public string Name { get; set; }
+    public int GroupId { get; set; }
+    public List<int> Grades { get; set; }
+
+    public override string ToString()
+    {
+        return $"ID группы ({GroupId}) {Name} {string.Join(",", Grades.ToArray())}";
+    }
+}
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        List<Order> orders = new List<Order>
+        {
+            new Order { OrderId = 1, CustomerName = "Анна", TotalAmount = 15000 },
+            new Order { OrderId = 2, CustomerName = "Иван", TotalAmount = 25000 },
+            new Order { OrderId = 3, CustomerName = "Мария", TotalAmount = 18000 }
+        };
+
+        List<Customer> customers = new List<Customer>
+        {
+            new Customer { Name = "Анна", Email = "anna@mail.ru", City = "Москва" },
+            new Customer { Name = "Иван", Email = "ivan@mail.ru", City = "Санкт-Петербург" },
+            new Customer { Name = "Мария", Email = "maria@mail.ru", City = "Москва" },
+            new Customer { Name = "Петр", Email = "petr@mail.ru", City = "Казань" }
+        };
+
+
+        Console.WriteLine("Задание 4\n"); // 44444444444444444
+
+        var CustomersOrders = from c in customers
+                              join o in orders on c.Name equals o.CustomerName
+                              select new
+                              {
+                                  Name = c.Name,
+                                  Email = c.Email,
+                                  City = c.City,
+                                  OrderId = o.OrderId,
+                                  TotalAmount = o.TotalAmount
+                              };
+
+        Console.WriteLine("Объединенные заказы с клиентами по имени и вывод по всем заказам");
+        foreach (var order in CustomersOrders)
+        {
+            Console.WriteLine($"ID {order.OrderId} : {order.Name} {order.Email} {order.City} {order.TotalAmount}");
+        }
+
+        var sumByCity = customers.GroupJoin(
+                                   orders,
+                                   c => c.Name,
+                                   o => o.CustomerName,
+                                   (c, os) => new
+                                   {
+                                       c.City,
+                                       Orders = os
+                                   })
+                                   .GroupBy(x => x.City)
+                                   .Select(g => new
+                                   {
+                                       City = g.Key,
+                                       TotalAmount = g.Sum(x => x.Orders.Sum(o => o.TotalAmount))
+                                   });
+
+        Console.WriteLine("\nОбщая сумма заказа для каждого города");
+
+        foreach (var o in sumByCity)
+        {
+            Console.WriteLine($"{o.City}: {o.TotalAmount}");
+        }
+
+
+        var customersWithNoOrders = customers.ExceptBy(orders.Select(o => o.CustomerName), c => c.Name);
+
+        Console.WriteLine("\nКлиенты без заказа:");
+        foreach (var c in customersWithNoOrders)
+        {
+            Console.WriteLine($"{c.Name} — {c.City}");
+        }
+
+
+
+        Console.WriteLine("\nЗадание 5\n"); // 555555555
+
+        List<Student> students = new List<Student>
+        {
+            new Student { Name = "Алексей", GroupId = 1, Grades = new List<int> { 5, 4, 5, 3 } },
+            new Student { Name = "Екатерина", GroupId = 1, Grades = new List<int> { 4, 4, 5, 5 } },
+            new Student { Name = "Дмитрий", GroupId = 2, Grades = new List<int> { 3, 4, 3, 4 } },
+            new Student { Name = "Светлана", GroupId = 2, Grades = new List<int> { 5, 5, 5, 5 } },
+            new Student { Name = "Михаил", GroupId = 1, Grades = new List<int> { 4, 3, 4, 4 } }
+        };
+
+
+        var averagePerStudent = from student in students
+                                let average = student.Grades.Average()
+                                select new { Name = student.Name, AVG = average };
+
+        Console.WriteLine("\nСредний балл для каждого студента:");
+        foreach (var student in averagePerStudent)
+        {
+            Console.WriteLine($"{student.Name} = {student.AVG}");
+        }
+
+
+
+        var groupedById = students.GroupBy(s => s.GroupId);
+
+        Console.WriteLine("\nСредний балл по группам и лучший ученик:");
+        foreach (var group in groupedById)
+        {
+            Console.WriteLine($"Id({group.Key}) средний = {group.SelectMany(s => s.Grades).Average()}," +
+                $" лучший = {group.MaxBy(s => s.Grades.Average())?.Name}");
+        }
+
+
+
+        var studentsWithFive = from student in students
+                               where student.Grades.Any(g => g == 5)
+                               select student;
+
+        Console.WriteLine("\nВсе студенты у которых хотя бы одна оценка 5:");
+        foreach (var student in studentsWithFive) Console.WriteLine(student.ToString());
+
+
+
+        var sortedStudents = students.OrderByDescending(s => s.Grades.Average());
+        Console.WriteLine("\nСортировка по убыванию среднего балла:");
+        foreach (var student in sortedStudents) Console.WriteLine(student.ToString());
+
+
+        var students2 = from student in students
+                        let average = student.Grades.Average()
+                        let gradesCount = student.Grades.Count()
+                        select new
+                        {
+                            Name = student.Name,
+                            GroupId = student.GroupId,
+                            AVG = average,
+                            GradesCount = gradesCount
+                        };
+
+        Console.WriteLine("\nОбновленный список с анонимным типом:");
+
+        foreach (var student in students2)
+        {
+            Console.WriteLine($"Id группы ({student.GroupId}), имя - {student.Name}, средний балл - {student.AVG} , кол-во оценок - {student.GradesCount}");
+        }
+
+    }
+}
